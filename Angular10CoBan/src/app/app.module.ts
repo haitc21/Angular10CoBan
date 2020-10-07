528b560c29f952fc697d808ecf81c93db8f363d0
haitc <haitc@HAITC_DES>
2020-10-07 17:19:45 +0700

    Bai3

--- Angular10CoBan/src/app/app.module.ts
+++ Angular10CoBan/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewCmpComponent } from './new-cmp/new-cmp.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      NewCmpComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
